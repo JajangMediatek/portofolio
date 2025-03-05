a9b8b1d230dcfb43f363cceee945079ef4eb0b04
jajang satoru <aldoyonandaf@gmail.com>
2025-03-05 11:36:56 +0700

    Update script.js
    Tambah animasi scroll certificate

--- script.js
+++ script.js
@@ -68,6 +68,6 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img img, .services-container, .project-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img img, .services-container, .project-box, .testimonial-wrapper, .contact form, .certificate-box', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
